4a186811cfb4c2612c14285f3721883d4bcdaeb2
keremgobekcioglu <keko1234keko@outlook.com>
2026-08-03 15:08:10 +0300

    first setup. .env file should be created and environment variables should be configured when cloning the repo.

---
+++ src/main/java/com/firstRestApi/demo/BookApi.java
@@ -0,0 +1,13 @@
+package com.firstRestApi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApi.class, args);
+	}
+
+}
