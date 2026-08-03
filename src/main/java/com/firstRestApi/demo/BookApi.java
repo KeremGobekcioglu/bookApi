7de27b27dac04ab00c8d080e6f251b3c85d199a4
keremgobekcioglu <keko1234keko@outlook.com>
2026-08-03 16:30:51 +0300

    Add CommandLineRunner to display database connection properties

--- src/main/java/com/firstRestApi/demo/BookApi.java
+++ src/main/java/com/firstRestApi/demo/BookApi.java
@@ -1,7 +1,10 @@
 package com.firstRestApi.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BookApi {
@@ -10,4 +13,11 @@ public class BookApi {
 		SpringApplication.run(BookApi.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(Environment env) {
+		return args -> {
+			System.out.println("DB URL = " + env.getProperty("spring.datasource.url"));
+			System.out.println("DB User = " + env.getProperty("spring.datasource.username"));
+		};
+	}
 }
